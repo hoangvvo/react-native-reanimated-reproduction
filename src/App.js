d669ea1272c1dc41721a59fce1abf28cb223be13
Hoang Vo <dev+g@hvvo.dev>
2021-09-17 17:14:25 +0700

    Add test case

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React from "react";
-import { Text, View } from "react-native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Text } from "react-native";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from "react-native-gesture-handler";
+import Animated, { useAnimatedGestureHandler } from "react-native-reanimated";
+
+const Test = () => {
+  const eventHandler = useAnimatedGestureHandler(
+    {
+      onActive(event) {
+        console.log("onActive");
+      },
+      onEnd() {
+        console.log("onEnd");
+      },
+      onFinish() {
+        console.log("onFinish");
+      },
+    },
+    []
+  );
+
+  return (
+    <PanGestureHandler onGestureEvent={eventHandler} maxPointers={1}>
+      <Animated.View
+        style={{ backgroundColor: "#001a72", padding: 24, marginTop: 12 }}
+      >
+        <Text style={{ color: "#ffffff" }}>Animated.View component</Text>
+      </Animated.View>
+    </PanGestureHandler>
+  );
+};
 
 const App = () => {
   return (
     <GestureHandlerRootView>
-      <View style={{ justifyContent: "center", flex: 1 }}>
-        <Text>Hello World</Text>
-      </View>
+      <Test />
     </GestureHandlerRootView>
   );
 };
